1dad7ed3142ae7618df84b07b4318a5eca833334
arunazturganbai <arunazturganbai@gmail.com>
2023-04-19 19:27:43 +0600

    My array list

---
+++ src/MyArrayList.java
@@ -0,0 +1,66 @@
+public class MyArrayList<T> implements MyList<T> {
+
+    private Object[] data;
+    private int size;
+
+    public MyArrayList() {
+        this.data = new Object[10]; // initial capacity of 10
+        this.size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+
+}
