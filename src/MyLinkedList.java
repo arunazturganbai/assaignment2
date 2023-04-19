fdfde30bf9747758540f02d55c0b113cb8cee473
arunazturganbai <arunazturganbai@gmail.com>
2023-04-19 23:03:16 +0600

    .

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,6 +1,6 @@
 public class MyLinkedList<T> implements MyList<T> {
 
-    private class Node {
+    private class Node{
         T element;
         Node next;
         Node prev;
@@ -8,63 +8,73 @@ public class MyLinkedList<T> implements MyList<T> {
         Node(T element) {
             this.element = element;
             this.next = null;
-            this.prev = null;
+            this.prev = null;}
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+        @Override
+        public int size() {
+            return 0;
         }
-    }
 
-    @Override
-    public int size() {
-        return 0;
-    }
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
 
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
+        @Override
+        public void add(T item) {
 
-    @Override
-    public void add(T item) {
+        }
 
-    }
+        @Override
+        public void add(T item, int index) {
 
-    @Override
-    public void add(T item, int index) {
+        }
 
-    }
+        @Override
+        public boolean remove(T item) {
+            return false;
+        }
 
-    @Override
-    public boolean remove(T item) {
-        return false;
-    }
+        @Override
+        public T remove(int index) {
+            return null;
+        }
 
-    @Override
-    public T remove(int index) {
-        return null;
-    }
+        @Override
+        public void clear() {
 
-    @Override
-    public void clear() {
+        }
 
-    }
+        @Override
+        public T get(int index) {
+            return null;
+        }
 
-    @Override
-    public T get(int index) {
-        return null;
-    }
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
 
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
 
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
+        @Override
+        public void sort() {
 
-    @Override
-    public void sort() {
+        }
 
     }
 
-}
